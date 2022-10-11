9f94a437a8cab4f7f8c2af457591d7fe729b2720
Teodor Linder <teodorl@gmail.com>
2022-10-11 13:15:26 +0200

    Add routing support to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../public/css/styles.css";
 import React from "react";
 import { StyleProvider, ThemePicker } from 'vcc-ui';
+import type { AppProps } from 'next/app'
 
-function HomePage() {
+function HomePage({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
       <StyleProvider>
         <ThemePicker variant="light">
+          <Component {...pageProps} />
         </ThemePicker>
       </StyleProvider>
     </React.StrictMode>
