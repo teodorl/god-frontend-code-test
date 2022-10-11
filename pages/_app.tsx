08d17cd411a7afed1acfc5cef0f232b573ac2a3c
Teodor Linder <teodorl@gmail.com>
2022-10-11 12:24:14 +0200

    Remove dummy HelloWorld component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
-import { HelloWorld } from "../src/components/HelloWorld";
 import "../public/css/styles.css";
 import React from "react";
 
 function HomePage() {
   return (
     <React.StrictMode>
-      <HelloWorld />
     </React.StrictMode>
   );
 }
