8aecfe59f4a8c4d22e29c3698859488abe4cbecd
Teodor Linder <teodorl@gmail.com>
2022-10-12 11:40:24 +0200

    Apply FilterBar on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Car } from '../interfaces/car';
 import { CarDetails } from '../components/CarDetails';
 import { CarDetailsActionBar } from '../components/CarDetailsActionBar';
 import { ProductCarousel } from '../components/ProductCarousel';
+import { FilterBar } from '../components/FilterBar';
 import { getCars } from '../helpers/api';
 import { LoadingBar } from 'vcc-ui';
 
 const Index = () => {
     const { data, error } = getCars();
+    const [activeFilter, setActiveFilter] = useState('');
 
     if (error) return <div>Failed to load cars</div>;
     if (!data) return <LoadingBar isLoading={true} />;
 
-    const sliderItems = data.map((car: Car, index: number) => {
+    const sliderItems = getFilteredItems(data, activeFilter)?.map((car: Car, index: number) => {
         return {
             id: index,
             renderItem: (
@@ -23,8 +25,22 @@ const Index = () => {
             ),
         };
     });
+    const bodyTypes = [...new Set(data.map((car: Car) => car.bodyType))] as string[];
+    bodyTypes.unshift('all');
 
-    return <ProductCarousel items={sliderItems} />;
+    return (
+        <>
+            <FilterBar items={bodyTypes} onFilterChange={setActiveFilter}></FilterBar>
+            <ProductCarousel items={sliderItems} />
+        </>
+    )
 };
+
+const getFilteredItems = (items: Car[], activeFilter: string) => {
+    if (!items) return [];
+    if (activeFilter === '' || activeFilter === 'all') return items;
+
+    return items.filter((item) => item.bodyType === activeFilter);
+}
 
 export default Index;
